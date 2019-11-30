f930ec452d5c2ccb461c18e4f21e9b4ffa0a9fc2
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-30 15:57:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,15 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0
+  while array[counter] < array.length do
+    puts array[counter]
+    counter += 1
+  end
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.sort[-1]
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.sort[0]
 end
