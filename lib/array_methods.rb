28dc9cd9b131766d157fcbd46f2d02103f02e727
Demarius Edwards <demarius.edwards@gmail.com>
2019-11-30 15:59:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,7 @@
 def find_element_index(array, value_to_find)
-  counter = 0
-  while array[counter] < array.length do
-    puts array[counter]
-    counter += 1
-  end
+  length = array.length
+  length.times do |index|
+    puts index
 end
 
 def find_max_value(array)
